cbae692eeed8a1cb04b1449889266bc7d0f8011f
Rota <sryota461@gmail.com>
2019-02-06 22:42:56 +0900

    Remove a method from constructor

--- inscription/settings.py
+++ inscription/settings.py
@@ -2,14 +2,12 @@
 import configparser
 import os
 
-class Settings:
+class Settings(object):
     def __init__(self):
         self.cdir = os.getcwd()
         self.config = configparser.ConfigParser()
         
         self.PATH = []
-
-        self.read_config()
 
     def read_config(self):
         conf_path = os.path.join(self.cdir, 'config.ini')
