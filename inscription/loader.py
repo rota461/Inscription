9d1c1b1d6f15031f49cab3aa9dad3bad475b9132
Rota <sryota461@gmail.com>
2019-02-09 21:46:34 +0900

    Update md_list method

--- inscription/loader.py
+++ inscription/loader.py
@@ -6,19 +6,19 @@ from inscription.settings import Settings
 
 class Loader(object):
 
-    def md_list(self, path):
+    def md_list(self, path, count):
         path = Path(path)
-        md_list = path.glob('*.md')
+        md_list = path.glob('*_*.md')
         md_list = [os.path.basename(x) for x in md_list]
-        return md_list
+        md_list = sorted(md_list, reverse=True)
+
+        if count == -1:
+            return md_list
+        else :
+            return md_list[0:count]
 
     def template_list(self, path):
         path = Path(path)
         template_list = path.glob('*_template.html')
         template_list = [os.path.basename(x) for x in template_list]
         return template_list
-
-    def md_sorted_list(self, path, count):
-        md_list = glob.glob(path + '/*.md')
-        md_sorted_list = sorted(md_list,key=os.path.getatime)
-        return md_sorted_list[0:count]
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -11,15 +11,18 @@ from inscription.loader import Loader
 class TestLoader(object):
 
     def test_md_list(self):
-        expected = ['test1.md','test2.md','test3.md']
+        expected_all = ['3_test3.md','2_test2.md','1_test1.md']
+        expected_top2 = ['3_test3.md','2_test2.md']
         loader = Loader()
 
         chdir = os.path.dirname(os.path.abspath(__file__))
         path = os.path.join(chdir,'contents\\articles\\')
 
-        actual = loader.md_list(path)
-
-        assert actual == expected
+        actual_all = loader.md_list(path, -1)
+        actual_top2 = loader.md_list(path, 2)
+          
+        assert actual_all == expected_all
+        assert actual_top2 == expected_top2
 
     def test_template_list(self):
         expected = ['test1_template.html','test2_template.html','test3_template.html']
