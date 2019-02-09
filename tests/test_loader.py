e15d85a313a49ebed1ebf49e43bb34e92545cbde
Rota <sryota461@gmail.com>
2019-02-09 20:47:04 +0900

    Add template_list test

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -13,9 +13,21 @@ class TestLoader(object):
     def test_md_list(self):
         expected = ['test1.md','test2.md','test3.md']
         loader = Loader()
+
         chdir = os.path.dirname(os.path.abspath(__file__))
         path = os.path.join(chdir,'contents\\articles\\')
 
         actual = loader.md_list(path)
+
+        assert actual == expected
+
+    def test_template_list(self):
+        expected = ['test1_template.html','test2_template.html','test3_template.html']
+        loader = Loader()
+
+        chdir = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(chdir, 'templates\\')
+        
+        actual = loader.template_list(path)
 
         assert actual == expected
