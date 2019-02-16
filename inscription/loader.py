ffa77f80a9664f8ded35bb750a1d02538966d5f8
Rota <sryota461@gmail.com>
2019-02-16 17:14:22 +0900

    Remove class because loader doesn't need to have states

--- inscription/loader.py
+++ inscription/loader.py
@@ -4,33 +4,30 @@ from pathlib import Path
 
 from inscription.settings import Settings
 
-class Loader(object):
+def md_list(path, count):
+    path = Path(path)
+    # entries or articles name must be yyyymmdd_serial_title.md.
+    md_list = path.glob('*_*_*.md')
+    md_list = [os.path.basename(x) for x in md_list]
+    md_list = sorted(md_list, reverse=True)
 
-    def md_list(self, path, count):
-        path = Path(path)
-        # entries or articles name must be yyyymmdd_serial_title.md.
-        md_list = path.glob('*_*_*.md')
-        md_list = [os.path.basename(x) for x in md_list]
-        md_list = sorted(md_list, reverse=True)
+    if count == -1:
+        return md_list
+    else :
+        return md_list[0:count]
+def template_list(path):
+    path = Path(path)
+    template_list = path.glob('*_template.html')
+    template_list = [os.path.basename(x) for x in template_list]
+    return template_list
 
-        if count == -1:
-            return md_list
-        else :
-            return md_list[0:count]
-
-    def template_list(self, path):
-        path = Path(path)
-        template_list = path.glob('*_template.html')
-        template_list = [os.path.basename(x) for x in template_list]
-        return template_list
-
-    def entries_dir_list(self, path):
-        path = Path(path)
-        tmp_list = path.glob('*')
-        dir_list = []
-        print(dir_list)
-        for x in tmp_list:
-            if os.path.isdir(x):
-                dir_list.append(os.path.basename(x))
-        dir_list = sorted(dir_list, reverse=True)
-        return dir_list
+def entries_dir_list(path):
+    path = Path(path)
+    tmp_list = path.glob('*')
+    dir_list = []
+    print(dir_list)
+    for x in tmp_list:
+        if os.path.isdir(x):
+            dir_list.append(os.path.basename(x))
+    dir_list = sorted(dir_list, reverse=True)
+    return dir_list
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -6,42 +6,39 @@ import pytest
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from inscription.loader import Loader
+import inscription.loader
 
 class TestLoader(object):
 
     def test_md_list(self):
         expected_all = ['20190213_3_test3.md','20190213_2_test2.md','20190213_1_test1.md']
         expected_top2 = ['20190213_3_test3.md','20190213_2_test2.md']
-        loader = Loader()
 
         chdir = os.path.dirname(os.path.abspath(__file__))
         path = os.path.join(chdir,'contents\\articles\\')
 
-        actual_all = loader.md_list(path, -1)
-        actual_top2 = loader.md_list(path, 2)
+        actual_all = inscription.loader.md_list(path, -1)
+        actual_top2 = inscription.loader.md_list(path, 2)
           
         assert actual_all == expected_all
         assert actual_top2 == expected_top2
 
     def test_template_list(self):
         expected = ['test1_template.html','test2_template.html','test3_template.html']
-        loader = Loader()
 
         chdir = os.path.dirname(os.path.abspath(__file__))
         path = os.path.join(chdir, 'templates\\')
         
-        actual = loader.template_list(path)
+        actual = inscription.loader.template_list(path)
 
         assert actual == expected
 
     def test_entries_dir_list(self):
         expected = ['201903', '201902', '201901']
-        loader = Loader()
 
         chdir = os.path.dirname(os.path.abspath(__file__))
         path = os.path.join(chdir, 'contents\\entries\\')
         
-        actual = loader.entries_dir_list(path)
+        actual = inscription.loader.entries_dir_list(path)
 
         assert actual == expected
